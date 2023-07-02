0536a373041ddf01eae1db1cc80235cc84b1c040
João Ferreira <joaobatistacomunic@hotmail.com>
2023-07-02 11:50:44 -0300

    projeto inicializado

---
+++ src/App/App.tsx
@@ -0,0 +1,15 @@
+import styles from './App.module.css';
+
+function App() {
+  return (
+    <div className={styles.App}>
+      <header className={styles.AppHeader}>
+        <p>
+          Hello.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
